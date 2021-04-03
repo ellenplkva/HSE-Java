717804492c62de9774d962ab94e121da815f73c7
Elena Polyakova <79382119+ellenplkva@users.noreply.github.com>
2021-04-03 23:25:43 +0300

    Add files via upload

---
+++ Lab4/src/Info.java
@@ -0,0 +1,22 @@
+public class Info {
+    private final String name;
+    private final String sername;
+    private final String patronymic;
+    private final String initials;
+    private final String date;
+    private final String sex;
+    private final int age;
+
+    public getInfo(){
+        
+    }
+
+    
+
+
+
+
+
+
+
+}
