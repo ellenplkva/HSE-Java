6ad822b68ce6e2f25518f4f246afbae1f6be10c7
Elena Polyakova <79382119+ellenplkva@users.noreply.github.com>
2021-04-04 11:15:41 +0300

    Update Main.java

--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        Info personeInfo = new Info();
+        personeInfo.getInfo();
+        personeInfo.gender();
 
     }
 }
